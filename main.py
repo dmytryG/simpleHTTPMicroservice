7bcdeb2044471d3c791c5bd4141b26859de90174
dmytryG <35425324+dmytryG@users.noreply.github.com>
2022-10-03 23:40:38 +0300

    Simple Hello world like handler

---
+++ main.py
@@ -0,0 +1,16 @@
+from aiohttp import web
+from aiohttp.web_routedef import Request
+
+
+# "Hello world" handler, it only reads name param from the get requests and answers with "Hello, {name}"
+# It's not really interesting, right?
+async def handle(request: Request):
+    name = request.query['name']
+    return web.Response(text="Hello, {name}!".format(name = name))
+
+app = web.Application()
+app.add_routes([web.get('/', handle),
+                web.get('/{name}', handle)])
+
+if __name__ == '__main__':
+    web.run_app(app)
